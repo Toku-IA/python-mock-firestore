01c11d1e2778dba4acc358514dc32af7ed7114e6
Samuel Goldfarb <sgoldfarb@trytoku.com>
2024-10-04 11:37:50 -0300

    📝 update readme, requirements and setup.py

--- setup.py
+++ setup.py
@@ -5,20 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mock-firestore",
-    version="0.11.0",
-    author="Matt Dowds",
+    version="0.11.1",
+    author="",
     description="In-memory implementation of Google Cloud Firestore for use in tests",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/mdowds/mock-firestore",
+    url="https://github.com/Toku-IA/python-mock-firestore",
     packages=setuptools.find_packages(),
-    test_suite='',
+    test_suite="",
     classifiers=[
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Programming Language :: Python :: 3.10",
         "License :: OSI Approved :: MIT License",
     ],
 )
